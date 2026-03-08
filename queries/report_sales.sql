d72ec1efe758e7c91cdf1268e756227a08287ba0
perezcalderondulcemariam3s1-gif <perez.calderon.dulce.maria.m3s1@gmail.com>
2026-03-08 15:18:53 -0600

    Update report_sales.sql

--- queries/report_sales.sql
+++ queries/report_sales.sql
@@ -1,18 +1,7 @@
--- Reporte Detallado de Ventas
+-- Resumen de Ventas por Tipo de Pago (Corte de Caja)
 SELECT 
-    s.sale_id AS 'Ticket',
-    s.sale_date AS 'Fecha',
-    u_cust.name AS 'Cliente',
-    u_emp.name AS 'Atendido por',
-    p.name AS 'Producto',
-    sd.quantity AS 'Cant.',
-    sd.unit_price AS 'Precio Unit.',
-    (sd.quantity * sd.unit_price) AS 'Subtotal'
-FROM SALES s
-JOIN CUSTOMERS c ON s.customer_id = c.customer_id
-JOIN USERS u_cust ON c.user_id = u_cust.user_id
-JOIN EMPLOYEES e ON s.employee_id = e.employee_id
-JOIN USERS u_emp ON e.user_id = u_emp.user_id
-JOIN SALES_DETAILS sd ON s.sale_id = sd.sale_id
-JOIN PRODUCTS p ON sd.product_id = p.product_id
-ORDER BY s.sale_date DESC;
+    payment_type AS 'Método de Pago',
+    COUNT(sale_id) AS 'Total Ventas',
+    SUM(total) AS 'Dinero Recaudado'
+FROM sales
+GROUP BY payment_type;
