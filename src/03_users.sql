f868f685447e9ae25b4b94c9550a772bdbf4e19c
perezcalderondulcemariam3s1-gif <perez.calderon.dulce.maria.m3s1@gmail.com>
2026-03-08 14:59:38 -0600

    Modify user creation and grant privileges in SQL
    Updated user creation and permissions in SQL script.

--- src/03_users.sql
+++ src/03_users.sql
@@ -1,7 +1,12 @@
--- Crear el usuario de la aplicación
-CREATE USER 'bakery_admin'@'localhost' IDENTIFIED BY 'Pastelito2026!';
+-- 1. Crear tu usuario (Sustituye 'tu_password' por una real)
+CREATE USER 'mielecita_admin'@'localhost' IDENTIFIED BY 'mar1234';
 
--- Darle permisos totales sobre la base de datos (suponiendo que se llama bakery_db)
-GRANT ALL PRIVILEGES ON bakery_db.* TO 'bakery_admin'@'localhost';
+-- 2. Crear el usuario de Wendy (Sustituye 'wendy_password' por una real)
+CREATE USER 'wendy_admin'@'localhost' IDENTIFIED BY 'wen1234';
 
+-- 3. Darles poder total sobre la base de datos 'bakery_db'
+GRANT ALL PRIVILEGES ON bakery_db.* TO 'mielecita_admin'@'localhost';
+GRANT ALL PRIVILEGES ON bakery_db.* TO 'wendy_admin'@'localhost';
+
+-- 4. Guardar cambios
 FLUSH PRIVILEGES;
